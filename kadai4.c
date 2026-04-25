a245861406130e60030cd93a939a12c23a83e708
optibeeprime <105331080+optibeeprime@users.noreply.github.com>
2026-04-25 16:20:01 +0900

    Create kadai4.c

---
+++ kadai4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int baseball = 9;
+    int volleyball = 6;
+    int total;
+
+    total = baseball + volleyball;
+
+    printf("合計人数 = %d人\n", total);
+
+    return 0;
+}
